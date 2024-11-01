5cbafc3ad0b2609ef9964c8f4ff4c0a71e4e9181
ArsalanAhmed92 <murtazayousufzai833@gmail.com>
2024-11-01 08:47:42 +0500

    class work

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import link from "react"
+
+const page = () => {
+  return (
+    <div>
+      <div  className='bg-gray-400'>
+        <div>Arsalan Ahmed </div>
+        <div className = "flex justify-between" >
+        <>Home</>
+        <>About us</>
+        <>contact us</>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default page
